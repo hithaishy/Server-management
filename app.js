4dcda2f71d596d8da409277f76e080dcdaa6e158
hithaishy bajje <hithaishy.jc@gmail.com>
2019-01-07 20:11:24 -0800

    create phase

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 //var todos = require('./routes/todos');
 const exec = require('child_process').exec;
@@ -10,12 +9,10 @@ const exec = require('child_process').exec;
 
 var app = express();
 // View Engine
+
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.engine('html',require('ejs').renderFile);
-
-
-app.use(express.static(path.join(__dirname,'client1')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
